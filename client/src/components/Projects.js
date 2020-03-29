7e62f03e989a750152d4849f90345957131fbbeb
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-03-29 15:16:59 -0400

    Started thumbnails for project section.

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import '../App.css';
+import Thumbnail from './Thumbnail';
+import VHGype from '../images/vgh.JPG';
+import P3 from '../images/p3thumbnail.JPG';
 import { Container } from 'reactstrap';
 
 class Projects extends Component {
   render() {
     return (
       <div>
-        <Container>Projects Page</Container>
+        <Container>
+          <Thumbnail
+            link='https://vghypeman.herokuapp.com/'
+            image={VHGype}
+            title='VGHypeman'
+          />
+
+          <Thumbnail
+            link='https://ca-company-tbd.herokuapp.com/'
+            image={P3}
+            title='LOADCo'
+          />
+        </Container>
       </div>
     );
   }
