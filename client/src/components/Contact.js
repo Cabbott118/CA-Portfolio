cce95baf7324c07c1819ee5201a6a73beefad8bf
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-04-03 12:29:40 -0400

    Styled contact form and added alert for success.

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Container } from 'reactstrap';
+import { Alert, Container, Button, Form, Label, Input } from 'reactstrap';
+
+const contactStyle = {
+  paddingTop: '3rem',
+  width: '100%',
+  backgroundColor: 'rgb(8, 8, 10)'
+};
 
 class Contact extends Component {
+  state = {
+    isOpen: false
+  };
+
   handleSubmit(e) {
     e.preventDefault();
     const name = document.getElementById('name').value;
@@ -16,45 +26,61 @@ class Contact extends Component {
         email: email,
         message: message
       }
-    }).then(document.getElementById('contact-form').reset());
+    }).then(
+      document.getElementById('contact-form').reset(),
+      this.setState({
+        isOpen: !this.state.isOpen
+      })
+    );
   }
 
   render() {
     return (
-      <div>
+      <div style={contactStyle}>
         <Container>
-          <form
+          <Form
             onSubmit={this.handleSubmit.bind(this)}
             id='contact-form'
             method='post'
           >
-            <label htmlFor='name'>Name</label>
-            <input
+            <Label htmlFor='name'>Name</Label>
+            <Input
+              className='mb-3'
               id='name'
               name='name'
               type='text'
-              placeholder='Your name'
+              placeholder='Enter your full name'
               required
             />
-            <label htmlFor='email'>Email</label>
-            <input
+            <Label htmlFor='email'>Email</Label>
+            <Input
+              className='mb-3'
               id='email'
               name='email'
               type='text'
-              placeholder='Your email'
+              placeholder='example@example.com'
               required
             />
-            <label htmlFor='message'>Message</label>
-            <textarea
+            <Label htmlFor='message'>Message</Label>
+            <Input
+              className='mb-3'
+              type='textarea'
               id='message'
               name='message'
               placeholder='Enter your message here'
-              rows='3'
               required
             />
-
-            <button type='submit'>Submit</button>
-          </form>
+            <Alert color='success' isOpen={this.state.isOpen}>
+              Thank you for reaching out, I will be getting back to you shortly!
+            </Alert>
+            <Button
+              className='mt-3 mb-3'
+              style={{ backgroundColor: '#45A29E' }}
+              type='submit'
+            >
+              Send
+            </Button>
+          </Form>
         </Container>
       </div>
     );
