53e74acbcbbdbb817696c2f31199cb5eba0fbe89
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-04-22 17:21:27 -0400

    Content for About section.

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -19,14 +19,16 @@ class Landing extends Component {
             </h1>
             <h4>Full Stack Web Developer</h4>
             <br />
-
             <h5 style={{ lineHeight: '1.2' }}>
-              I hold a certificate in Full Stack Web Development from the
-              University of Central Florida. Technology has always excited my
-              attention, which led me to pursue it as a career path. I am hungry
-              for knowledge and am willing to try new things to further develop
-              my skill set. If you would like me to join your team, or simply
-              see what I might be able to offer you, feel free to reach out!
+              Having a background in the hospitality industry, client
+              satisfaction is my number one goal.
+            </h5>
+            <h5 style={{ lineHeight: '1.2' }}>
+              Technology has always excited my attention, which led me to pursue
+              it as a career path. I am hungry for knowledge and am willing to
+              try new things to further develop my skill set. If you would like
+              me to join your team, or simply see what I might be able to offer
+              you, feel free to reach out!
             </h5>
           </div>
         </Container>
