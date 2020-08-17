f20bd9ac446ceddbd4a4acecba6dd9c99d1759ef
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-08-17 11:00:16 -0400

    About me section filled.

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -20,12 +20,14 @@ const styles = (theme) => ({
       paddingTop: '100px',
     },
   },
-  innerContainer: {},
+  innerContainer: {
+    // margin: '.5rem 0',
+  },
   myImg: {
     borderRadius: '30px',
     width: '250px',
     [theme.breakpoints.down('xs')]: {
-      width: '150px',
+      // width: '150px',
     },
   },
 });
@@ -44,7 +46,7 @@ function About(props) {
           direction='column'
           justify='center'
           alignItems='center'
-          spacing={0}
+          spacing={4}
           className={classes.gridContainer}
         >
           <Grid item>
@@ -63,21 +65,98 @@ function About(props) {
                 <img
                   src={myImg}
                   alt='Me'
-                  sm={12}
-                  md={4}
+                  xs={12}
+                  // md={6}
                   className={classes.myImg}
                 />
               </Grid>
-              <Grid item sm={12} md={8}>
+              {/* <Grid item sm={12} md={6}>
                 <Typography color='secondary' variant='h6'>
-                  Under Construction ---
+                  Hi, I'm Caleb Abbott, a Web Developer based out of Lakeland,
+                  Fl.
+                </Typography>
+              </Grid> */}
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid
+              container
+              direction='row'
+              justify='center'
+              alignItems='flex-start'
+              spacing={4}
+              className={classes.innerContainer}
+            >
+              <Grid item sm={12} md={3}>
+                <Typography
+                  color='primary'
+                  variant='h6'
+                  style={{
+                    marginBottom: '.5rem',
+                  }}
+                >
+                  Who Am I?
                 </Typography>
                 <Typography color='secondary' variant='body1'>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book.
+                  From a young age, I've been interested in how things work, why
+                  they work, and not only how I can keep them working, but
+                  improve them.
+                </Typography>
+                <Typography color='secondary' variant='body1'>
+                  I believe this is why I've been lead to become a Web
+                  Developer. It doesn't matter if I'm continuing work on
+                  something I started months ago, or picking up a brand new
+                  shiny project; each and every day brings new challenges and
+                  pushes me to continue learning and growing.
+                </Typography>
+              </Grid>
+              <Grid item sm={12} md={3}>
+                <Typography
+                  color='primary'
+                  variant='h6'
+                  style={{
+                    marginBottom: '.5rem',
+                  }}
+                >
+                  Where Am I Today?
+                </Typography>
+                <Typography color='secondary' variant='body1'>
+                  Today, I am a Golf Services Lead at the world-class golf
+                  resort, Streamsong. I work with an incredible group of people,
+                  from all walks of life, who come together as a team to
+                  showcase our spectacular resort.
+                </Typography>
+                <Typography color='secondary' variant='body1'>
+                  I have been fortunate to learn under a management team that
+                  has fostered my leadership skills and given me opportunity to
+                  grow as a man.
+                </Typography>
+              </Grid>
+              <Grid item sm={12} md={3}>
+                <Typography
+                  color='primary'
+                  variant='h6'
+                  style={{
+                    marginBottom: '.5rem',
+                  }}
+                >
+                  Where Will I Be Tomorrow?
+                </Typography>
+                <Typography color='secondary' variant='body1'>
+                  Tomorrow, I will continue to grow as a developer, a problem
+                  solver, a leader, and a man, by applying what I've learned
+                  today.
+                </Typography>
+                <Typography
+                  color='secondary'
+                  variant='subtitle1'
+                  style={{ marginTop: '.75rem' }}
+                >
+                  "You are today where your thoughts have brought you: you will
+                  be tomorrow where your thoughts take you."
+                </Typography>
+                <Typography color='secondary' variant='subtitle1'>
+                  - James Allen
                 </Typography>
               </Grid>
             </Grid>
