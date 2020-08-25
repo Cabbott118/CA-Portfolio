38acc3c06e85f4896fe90c4d2d7773b5ae8902c2
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-08-25 11:14:30 -0400

    Setting static inside prod build.

--- server.js
+++ server.js
@@ -11,13 +11,12 @@ app.use(cors());
 app.use('/api/contact', require('./routes/api/contact'));
 
 if (process.env.NODE_ENV === 'production') {
+  app.use('/routes/api/contact', express.static('contact'));
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
   });
 }
-
-app.use('/routes/api/contact', express.static('contact'));
 
 const port = process.env.PORT || 5000;
 
