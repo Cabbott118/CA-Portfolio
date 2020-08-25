5ad1b87352edad2c54817a0452f690e534bb1763
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-08-25 10:57:07 -0400

    Contact static file fix.

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+app.use('/routes/api/contact', express.static('contact'));
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
