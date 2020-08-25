be400a4ce7de3a98a12c298a2129b3ade68dbf41
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-08-25 12:01:11 -0400

    Reverted static files

--- server.js
+++ server.js
@@ -8,15 +8,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
-app.use('/api/contact', require('./routes/api/contact'));
-
 if (process.env.NODE_ENV === 'production') {
-  app.use('/routes/api/contact', express.static('contact'));
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
   });
 }
+
+app.use('/api/contact', require('./routes/api/contact'));
 
 const port = process.env.PORT || 5000;
 
